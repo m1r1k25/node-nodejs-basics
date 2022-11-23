f7d09bef05bd284b083445d82413302f978f4134
m1r1k77 <m1r1kzaitsev77@gmail.com>
2022-11-23 21:49:36 +0300

    add solution for args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,9 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2)
+    const result = args.reduce((acc, cur, index) => {
+        return [...acc, `${cur.slice(2)} is value${index + 1}` ]
+    }, []).join(', ')
+    console.log(result)
 };
 
 parseArgs();
