c19e7c3e757658baebdbc02b7a21ccac5c32f27c
m1r1k77 <m1r1kzaitsev77@gmail.com>
2022-11-23 17:34:46 +0300

    add solutions for copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,37 @@
+import fs from 'fs/promises'
+
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+import path from 'path'
+const pathToNewFolder = path.join(__dirname, 'files-copy');
+const pathToStartFolder = path.join(__dirname, 'files');
+
+const errorText = 'FS operation failed'
+
 const copy = async () => {
-    // Write your code here 
+    try {
+        await fs.access(pathToNewFolder)
+        throw new Error(errorText);
+    } catch(err) {
+        try {
+            if(err.message === errorText) throw new Error(errorText);
+            await fs.access(pathToStartFolder)
+            await fs.mkdir(pathToNewFolder);
+            const files = await fs.readdir(pathToStartFolder);
+            
+            files.forEach(async (file) => {
+                const startFile = path.join(__dirname, 'files', file);
+                const newFile = path.join(__dirname, 'files-copy', file);
+                await fs.copyFile(startFile, newFile);
+            });
+            console.log('Files had been coppied succesfully')
+        }
+        catch {
+            throw new Error(errorText)    
+        }
+    }
 };
 
 copy();
