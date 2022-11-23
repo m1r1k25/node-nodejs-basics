991e096e8ae41d1a43dafe1130b33eaa44e3f662
m1r1k77 <m1r1kzaitsev77@gmail.com>
2022-11-23 19:15:00 +0300

    add solution for env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
 const parseEnv = () => {
-    // Write your code here 
+    const allEnv = process.env
+    const rssKeys = Object.keys(allEnv).filter((key) => key.startsWith('RSS_'))
+    const result = rssKeys.reduce((acc, cur) => {
+        return [...acc, `${cur}=${allEnv[cur]}`]
+    }, []).join('; ')
+    console.log(result)
 };
 
 parseEnv();
