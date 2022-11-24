e5c21f5fbf480e6011ff8613cdf3c88e31cb0a68
m1r1k77 <m1r1kzaitsev77@gmail.com>
2022-11-24 22:03:39 +0300

    add solution for read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,15 @@
+import { promises as fs, createReadStream } from "fs"
+
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+import path from 'path'
+const pathToFile = path.join(__dirname, 'files', 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+    const readableStream = createReadStream(pathToFile)   
+    readableStream.pipe(process.stdout) 
 };
 
 await read();
