de37c12a0c8b001bcf3e928092ef55a7c51d8c8e
m1r1k77 <m1r1kzaitsev77@gmail.com>
2022-11-24 22:57:04 +0300

    add solution for write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,17 @@
+import { promises as fs, createWriteStream } from "fs"
+
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+import path from 'path'
+const pathToFile = path.join(__dirname, 'files', 'fileToWrite.txt');
+
 const write = async () => {
-    // Write your code here 
+    const stream = createWriteStream(pathToFile);
+    process.stdin.on('data', (chunk) => {
+      stream.write(chunk);
+    });
 };
 
 await write();
