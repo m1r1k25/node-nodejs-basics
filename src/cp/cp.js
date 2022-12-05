b33da44cd0e47ac32f39f30b9f2e5d3ea744210b
m1r1k77 <m1r1kzaitsev77@gmail.com>
2022-12-05 11:41:16 +0300

    add solution for cp.js

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,22 @@
+import { spawn } from 'child_process'
+
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+import path from 'path'
+const pathToScript = path.join(__dirname, 'files', 'script.js');
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+
+    const child = spawn('node', [pathToScript, ...args.split(' ')])
+
+    process.stdin.on('data', msg => child.stdin.write(msg))
+    
+    child.stdout.on('data', (data) => {
+        console.log(String(data))
+        console.log('some message \n')
+    })
 };
 
-spawnChildProcess();
+spawnChildProcess('--arg1 --arg2');
